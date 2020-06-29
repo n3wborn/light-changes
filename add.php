49ca3a12c0fe5d2d50be28f7b20a618b61f00c82
Stephane Pluchart <n3wborn@pm.me>
2020-06-29 11:03:52 +0200

    Update add.php structure to comply with new database

--- add.php
+++ add.php
@@ -1,42 +1,63 @@
 <?php require_once "dbconnect.php" ?>
 <?php require_once "header.php" ?>
 
-	<main id="main-container" class="container fcol">
-		<div id="form-container">
+<?php
+
+/*
+
+name="date" -> appartments.last_change
+name="floor" -> 	appartments.floor
+name="puissance"	-> light_power
+name="marque"  -> light_brand
+
+*/
+
+$date = '';
+$location = '';
+$floor = '';
+$power = '';
+$brand = '';
+
+//print_r($_POST);
+foreach ($_POST as $key => $value) {
+	var_dump($value);
+}
+
+
+
+?>
+
+
+<!-- Page Structure -->
+<main id="main-container" class="container fcol">
+	<div id="form-container">
 		<h2>Add panel</h2>
 		<p>Fill this form to submit new values</p>
-			<form action="" method="post" class="fcol">
-				<label for="date">Date</label>
-				<input type="date" name="date" class="form-input" required>
+		<!-- Main form -->
+		<form action="" method="post" class="fcol">
+			<label for="date">Date</label>
+			<input type="date" name="date" class="form-input" required>
 
-        <label for="position">Location</label>
-				<select id="position" class="form-input" name="position" required>
-        	<option value="" selected>Choose Location</option>
-					<option value="gauche">left</option>
-					<option value="droite">right</option>
-					<option value="fond">back</option>
-				</select>
-				<label for="etage">Floor</label>
-				<select id="etage" class="form-input" name="etage" required>
-					<option value="0" selected>0</option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-				</select>
-				<label for="puissance">Power</label>
-				<select id="puissance" class="form-input" name="puissance" required>
-					<option value="15" selected>15W</option>
-					<option value="20">20W</option>
-					<option value="25">25W</option>
-				</select>
-				<label for="marque">Brand</label>
-				<select id="marque" class="form-input" name="marque" required>
-					<option value="Philips" selected>Philips</option>
-					<option value="Sylvania">Sylvania</option>
-					<option value="Osram">Osram</option>
-					<option value="GE-Lighting">GE-Lighting</option>
-				</select>
+			<label for="location">Location</label>
+			<select id="location" class="form-input" name="location" required>
+				<option value="" selected>Choose Location</option>
+				<option value="gauche">left</option>
+				<option value="droite">right</option>
+				<option value="fond">back</option>
+			</select>
 
+			<label for="floor">Floor</label>
+			<input type="text" name="floor" class="form-input" required>
+
+			<label for="power">Power</label>
+			<input type="text" name="power" class="form-input" required>
+
+			<label for="brand">Brand</label>
+			<input type="text" name="brand" class="form-input" required>
+
+			<div class="container submit-container">
+				<button type="submit" class="btn btn-primary">Add</button>
+			</div>
 		</form>
 	</div>
+<!-- Page Structure -->
