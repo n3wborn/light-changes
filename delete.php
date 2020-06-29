f3a3c6cee754138e058d3fc5a229f435534f6f74
Stephane Pluchart <n3wborn@pm.me>
2020-06-29 19:48:13 +0200

    Add delete.php

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php require_once "dbconnect.php" ?>
+<?php require_once "header.php" ?>
+<?php require_once "functions.php" ?>
+
+<?php
+
+//we handle GET request
+$id = $_GET['id'];
+
+//if "seems to be an int"
+if (set_intstring($id)){
+	//prepare to delete
+	$sql = 'DELETE FROM appartments WHERE ID = :id';
+	$req = $pdo->prepare($sql);
+	//specify an int to avoi script k1ddi3s
+	$req->bindParam(':id',  $id, PDO::PARAM_INT);
+	//exec and go back to index
+	$req->execute();
+	header('Location: index.php');
+}
+
+?>
