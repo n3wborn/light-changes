2fea216ee2baf9dcb8ed48f298d6dc905bedcafc
n3wborn <n3wborn@protonmail.com>
2020-07-01 12:24:20 +0200

    remove modal button

--- index.php
+++ index.php
@@ -4,20 +4,6 @@
 
 <main id="main-container" class="fcol">
 
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
-
 	<table class="table table-primary">
 		<thead>
 			<tr>
