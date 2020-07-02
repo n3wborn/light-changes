ddd309aa12e08481139fa53cc514151c5bf285b2
n3wborn <n3wborn@protonmail.com>
2020-07-02 20:37:58 +0200

    Import users table to database dump

--- ligths.sql
+++ ligths.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: fd9c3c72d354
--- Generation Time: Jun 26, 2020 at 08:54 AM
--- Server version: 10.4.13-MariaDB-1:10.4.13+maria~bionic
--- PHP Version: 7.4.6
+-- Hôte : mariadb
+-- Généré le : jeu. 02 juil. 2020 à 18:36
+-- Version du serveur :  10.4.13-MariaDB-1:10.4.13+maria~focal
+-- Version de PHP : 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,142 +18,88 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ligths`
+-- Base de données : `datas`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ampoules`
---
-
-CREATE TABLE `ampoules` (
-  `ID` int(9) NOT NULL,
-  `marque` varchar(20) NOT NULL,
-  `puissance` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `ampoules`
---
-
-INSERT INTO `ampoules` (`ID`, `marque`, `puissance`) VALUES
-(1, 'Philips', '15W'),
-(2, 'Sylvania', '20W'),
-(3, 'Osram', '25W'),
-(4, 'GE-Lighting', '20W');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `appartments`
+-- Structure de la table `appartments`
 --
 
 CREATE TABLE `appartments` (
   `ID` int(9) NOT NULL,
-  `etage` varchar(2) NOT NULL,
-  `position` varchar(10) NOT NULL,
-  `changement` date NOT NULL
+  `floor` varchar(2) NOT NULL,
+  `location` varchar(10) NOT NULL,
+  `last_change` date NOT NULL,
+  `light_power` varchar(5) NOT NULL,
+  `light_brand` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `appartments`
+-- Déchargement des données de la table `appartments`
 --
 
-INSERT INTO `appartments` (`ID`, `etage`, `position`, `changement`) VALUES
-(1, '0', 'gauche', '2020-03-03'),
-(2, '0', 'droite', '2019-12-02'),
-(3, '1', 'gauche', '2020-03-21'),
-(4, '1', 'droite', '2020-06-03'),
-(5, '2', 'fond', '2019-07-07'),
-(6, '2', 'gauche', '2020-06-05'),
-(7, '3', 'gauche', '2020-06-21'),
-(8, '3', 'droite', '2020-03-09'),
-(9, '3', 'fond', '2020-06-01'),
-(10, '4', 'droite', '2020-02-06'),
-(11, '4', 'gauche', '2020-04-06'),
-(12, '4', 'fond', '2019-10-10');
+INSERT INTO `appartments` (`ID`, `floor`, `location`, `last_change`, `light_power`, `light_brand`) VALUES
+(3, '10', 'gauche', '2020-07-14', '40W', 'Osram'),
+(7, '3', 'gauche', '2020-06-21', '25W', 'Philipps'),
+(9, '10', 'gauche', '2020-07-14', '45W', 'Osram'),
+(11, '2', 'fond', '2020-07-10', '50W', 'Osram'),
+(13, '6', 'gauche', '2020-07-02', '40W', 'GE-Lightings'),
+(14, '6', 'droite', '2020-07-31', '40W', 'Philipps'),
+(16, '10', 'gauche', '2020-07-02', '25W', 'GE-lightings'),
+(19, '9', 'fond', '2020-07-16', '50W', 'Philipps');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `proprietaires`
+-- Structure de la table `users`
 --
 
-CREATE TABLE `proprietaires` (
-  `ID` int(9) NOT NULL,
-  `nom` varchar(100) NOT NULL,
-  `prenom` varchar(100) NOT NULL,
-  `appartment_id` int(9) NOT NULL
+CREATE TABLE `users` (
+  `ID` int(3) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `passwd` varchar(60) NOT NULL COMMENT 'create_passwd() salted hashes'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `proprietaires`
+-- Déchargement des données de la table `users`
 --
 
-INSERT INTO `proprietaires` (`ID`, `nom`, `prenom`, `appartment_id`) VALUES
-(3, 'Tesla', 'Nikolas', 1),
-(4, 'Einstein', 'Albert', 2),
-(5, 'de Lavoisier', 'Antoine', 3),
-(6, 'Pascal', 'Blaise', 4),
-(7, 'Mendeleiev', 'Dimitri', 5),
-(8, 'Schrodinger', 'Erwin', 6),
-(9, 'Galilée', 'Galiléo', 9),
-(10, 'Newton', 'Isaac', 8);
+INSERT INTO `users` (`ID`, `login`, `passwd`) VALUES
+(1, 'admin', '$2y$10$njOT.D47/2WV1G7ua/JWpuIPVNAFIVXso4mfw7TxHGdgQVwF1JoQy');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `ampoules`
---
-ALTER TABLE `ampoules`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `appartments`
+-- Index pour la table `appartments`
 --
 ALTER TABLE `appartments`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `proprietaires`
+-- Index pour la table `users`
 --
-ALTER TABLE `proprietaires`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `ampoules`
---
-ALTER TABLE `ampoules`
-  MODIFY `ID` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `appartments`
+-- AUTO_INCREMENT pour la table `appartments`
 --
 ALTER TABLE `appartments`
-  MODIFY `ID` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `ID` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- AUTO_INCREMENT for table `proprietaires`
+-- AUTO_INCREMENT pour la table `users`
 --
-ALTER TABLE `proprietaires`
-  MODIFY `ID` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `proprietaires`
---
-ALTER TABLE `proprietaires`
-  ADD CONSTRAINT `proprietaires_ibfk_1` FOREIGN KEY (`appartment_id`) REFERENCES `appartments` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `users`
+  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
